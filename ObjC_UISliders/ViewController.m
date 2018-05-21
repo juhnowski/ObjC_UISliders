e08e9b7697bdacd9928999de5f5a4f324c138cc4
Juhnowski <juhnowski@gmail.com>
2018-05-21 17:32:48 +0300

    Initial Commit

---
+++ ObjC_UISliders/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ObjC_UISliders
+//
+//  Created by SBT on 21/05/2018.
+//  Copyright © 2018 SBT. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
